0094056963506e28c324762d582d8f53f38ee20b
steve7329 <71615932+steve7329@users.noreply.github.com>
2026-09-18 10:27:17 +0900

    Add files via upload

---
+++ pages/9_session.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title('카운터 예제 : session_state 활용')
+# count = 0
+# if st.button('증가'):
+#     count += 1
+
+# st.write('count=', count)
+
+# session_state 사용
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+def increase():
+        st.session_state.count += 1
+
+st.button('증가', on_click=increase)
+
+st.write('count=', st.session_state.count)
+
+# if st.button('감소'):
+#     st.session_state.count -= 1
+
+# st.write('count=', st.session_state.count)
